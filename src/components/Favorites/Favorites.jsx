45982d8ad0f9a94caac97735e41a618e1254f73e
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2023-03-02 13:53:17 -0600

    favorite component setup

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,7 +1,32 @@
-function Favorites () {
-    return(
-        <h2>Favorites Page</h2>
-    )
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+function Favorites() {
+    const dispatch = useDispatch();
+    const favoritesReducer = useSelector(store => store.WHATS_THE_STORY_MOURNING_MAURY);
+
+    useEffect(() => {
+        getFavorites();
+    }, []);
+
+    const getFavorites = () => {
+        dispatch({
+            type: 'GET_FAVORITES'
+        });
+    };
+
+    return (
+        <>
+            <h2>Favorites Page</h2>
+            {favoritesReducer.map((favorite, i) => {
+                return <div key={i}>
+                    <img src={favorite} alt="One of your favorite gifs" />
+                </div>;
+            })}
+
+
+        </>
+    );
 }
 
-export default Favorites
+export default Favorites;
