a65be4eb82e68611381ce32cb671e01beb3ac035
Nolen Lawton <mnlawton@icloud.com>
2023-03-02 14:42:57 -0600

    set up base search file without any working update

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,6 +1,35 @@
+import { useSelector } from "react-redux"
+import { useDispatch } from "react-redux"
+
 function Search () {
+    const gifs = useSelector(store => store.setGifs)
+    const dispatch = useDispatch()
+
+    const getGifs = () => {
+        dispatch({
+            type: 'GET_GIFS',
+        })
+    }
+
+    const favoriteGif = () => {
+        console.log('favorite')
+    }
+    
     return(
-        <h2>Search Page</h2>
+        <>
+            <h2>Search Page</h2>
+            <input type='text' placeholder="search" />
+            <button onClick={getGifs}>Search Gifs</button>
+            {gifs.map((gif, i) => {
+                return(
+                    <div key={i}>
+                        <img src={gif} />
+                        <button onClick={favoriteGif} >Favorite</button>
+                    </div>
+                )
+            }
+            )}
+        </>
     )
 }
 
